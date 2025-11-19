5910b971b877e7338c0a563cef7cf504656a53fc
Sayd Alexandro Quevedo Flores <243701@ids.upchiapas.edu.mx>
2025-11-19 12:45:47 -0600

    Home View

--- app/src/main/java/com/sayd/notaudio/Navigation/NavGraph.kt
+++ app/src/main/java/com/sayd/notaudio/Navigation/NavGraph.kt
@@ -4,15 +4,18 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.sayd.notaudio.ui.Screens.HomeScreen
 import com.sayd.notaudio.ui.Screens.RegisterScreen
 
 @Composable
 fun NavGraph() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "register") {
+    NavHost(navController = navController, startDestination = "home") {
         composable("register") {
             RegisterScreen()
         }
-        // TODO: Add other destinations
+        composable("home") {
+            HomeScreen()
+        }
     }
 }
